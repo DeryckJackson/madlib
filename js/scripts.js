db0a79891444c6d4e295c01be2f369b4458f5a6c
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-03 09:21:55 -0700

    add adverb and adjective input
    Co-authored-by: Ian Scott <chesnekov@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,18 @@ $(document).ready(function() {
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
+    const adverbInput = $("input#verb").val();
     const verbInput = $("input#verb").val();
+    const adjInput = $("input#adj").val();
     const nounInput = $("input#noun").val();
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
+    $(".adverb").text(adverbInput);
     $(".verb").text(verbInput);
+    $(".adj").text(adjInput);
     $(".noun").text(nounInput);
 
     $("#story").show();
